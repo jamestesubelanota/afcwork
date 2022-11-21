05bde0cd57c09d97505e52433118bd892881fbff
james arturo <jamesarturo1997@gmail.com>
2022-11-21 10:44:07 -0500

    prueba

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $this->app->bind('path.public', function() {
-            return base_path().'/../public_html';
-        });
     }
 }
