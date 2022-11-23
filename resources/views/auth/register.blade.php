e16274e60f1ecd9cf6bf68fe45f4d5f033b0ec12
james arturo <jamesarturo1997@gmail.com>
2022-11-23 09:51:44 -0500

    se añaden etiqueta DE TOQUEN CSRF

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,6 +20,8 @@
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('register') }}">
+          
+
             @csrf
 
             <!-- Name -->
